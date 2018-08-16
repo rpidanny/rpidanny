39fe8526ee2e5e96252827c6434ae177138d0a79
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-17 00:23:56 +0200

    Style work component and its childs

--- src/containers/Common/Company/index.js
+++ src/containers/Common/Company/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 import { VerticalTimelineElement } from 'react-vertical-timeline-component'
-
+import './styles.css'
 class Company extends Component {
   constructor (props) {
     super(props)
@@ -18,17 +18,32 @@ class Company extends Component {
   render () {
     const {company, startDate, endDate, position, summary, website} = this.state.data
     return (
-      <VerticalTimelineElement
-        className='vertical-timeline-element--work'
-        date={`${startDate} - ${endDate}`}
-        iconStyle={{ background: '#2d2d2d', color: '#fff', width: '30px', height: '30px' }}
-        icon={<Glyphicon glyph='star' />}
-      >
-        <h3 className='vertical-timeline-element-title'><a href={website}>{company}</a></h3>
-        <h4 className='vertical-timeline-element-subtitle'>{position}</h4>
-        <p>
-          {summary}
-        </p>
+      // <VerticalTimelineElement
+      //   className='vertical-timeline-element--work'
+      //   iconStyle={{ background: '#2d2d2d', color: '#fff', width: '30px', height: '30px' }}
+      //   icon={<Glyphicon glyph='star' />}
+      // >
+      //   <h3 className='vertical-timeline-element-title'><a href={website}>{company}</a></h3>
+      //   <h4 className='vertical-timeline-element-subtitle'>{position}</h4>
+      //   <span className='vertical-timeline-element-date2'>{`${startDate} - ${endDate}`}</span>
+      //   <p className='vertical-timeline-element-summary'>
+      //     {summary}
+      //   </p>
+      // </VerticalTimelineElement>
+      <VerticalTimelineElement iconStyle={{display: 'none'}}>
+        <div className='work-item'>
+          <div className='work-ico'>
+            <Glyphicon glyph='briefcase' />}
+          </div>
+          <div className='work-desc'>
+            <h4>
+              <a href={website}>{company}</a>
+            </h4>
+            <h5>{position}</h5>
+            <h6>{startDate} - {endDate}</h6>
+            <p>{summary}</p>
+          </div>
+        </div>
       </VerticalTimelineElement>
       // <div className='col-md-6'>
       //   <div className='work-item'>
--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Work from '../Common/Work'
 import Education from '../Common/Education'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 
 class Resume extends Component {
   constructor (props) {
@@ -18,19 +19,14 @@ class Resume extends Component {
         <section className='row resume' id='resume'>
           <div className='col-md-12 title'>
             <h2>
-              <span className='glyphicon glyphicon-briefcase' /> / Resume
+              <Glyphicon glyph='briefcase' />} / Resume
             </h2>
           </div>
-          {/* Experience */}
           <div className='col-md-12'>
             <div className='content'>
               <Work companies={work} />
-
-              {/* mY Education.... */}
               <div className='divider'>
-                <span>
-                  <i className='glyphicon glyphicon-star' />
-                </span>
+                <Glyphicon glyph='star' />}
               </div>
               <Education institutions={education} />
             </div>
