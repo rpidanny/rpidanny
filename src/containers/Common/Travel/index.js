011ea3e2c2574877062fe8884363760d2dca6e4e
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-23 20:29:15 +0200

    Add section title and texts

--- src/containers/Common/Travel/index.js
+++ src/containers/Common/Travel/index.js
@@ -4,7 +4,9 @@ import {
   Geographies,
   Geography
 } from 'react-simple-maps'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 import worldMap from '../../../assets/world-50m.json'
+import './styles.css'
 
 const wrapperStyles = {
   width: '100%',
@@ -43,51 +45,64 @@ class Travel extends Component {
     if (visitedCountries.length > 0) {
       return (
         <React.Fragment>
-          <section className='row travel' >
-            <div style={wrapperStyles}>
-              <ComposableMap
-                projectionConfig={{
-                  scale: 180,
-                  rotation: [-11, 0, 0]
-                }}
-                width={980}
-                height={551}
-                style={{
-                  width: '100%',
-                  height: 'auto'
-                }}
-              >
-                <Geographies geography={worldMap} >
-                  {(geographies, projection) => geographies.map((geography, i) => (
-                    <Geography
-                      key={i}
-                      geography={geography}
-                      projection={projection}
-                      onClick={this.handleClick}
-                      style={{
-                        default: {
-                          fill: this.getFillColor(geography.properties.NAME, visitedCountries),
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none'
-                        },
-                        hover: {
-                          fill: this.getHoverColor(geography.properties.NAME, visitedCountries),
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none'
-                        },
-                        pressed: {
-                          fill: '#263238',
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none'
-                        }
-                      }}
-                    />
-                  ))}
-                </Geographies>
-              </ComposableMap>
+          <section className='row travel' id='travel' >
+            <div className='col-md-12 title'>
+              <h2>
+                <Glyphicon glyph='plane' /> / Travel
+              </h2>
+            </div>
+            <div className='col-md-12'>
+              <div className='content travelMap' >
+                <div className='inner-title'>
+                  <p>
+                    Countries visited so far: <i><b>{visitedCountries.length}</b></i>
+                  </p>
+                </div>
+                <ComposableMap
+                  projectionConfig={{
+                    scale: 220,
+                    rotation: [0, 0, 0],
+                    yOffset: 70
+                  }}
+                  width={980}
+                  height={551}
+                  style={{
+                    width: '100%',
+                    height: 'auto'
+                  }}
+                >
+                  <Geographies geography={worldMap} >
+                    {(geographies, projection) => geographies.map((geography, i) => (
+                      <Geography
+                        key={i}
+                        geography={geography}
+                        projection={projection}
+                        onClick={this.handleClick}
+                        style={{
+                          default: {
+                            fill: this.getFillColor(geography.properties.NAME, visitedCountries),
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          },
+                          hover: {
+                            fill: this.getHoverColor(geography.properties.NAME, visitedCountries),
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          },
+                          pressed: {
+                            fill: '#263238',
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          }
+                        }}
+                      />
+                    ))}
+                  </Geographies>
+                </ComposableMap>
+              </div>
             </div>
           </section>
         </React.Fragment>
