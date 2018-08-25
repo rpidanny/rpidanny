28c9d248e141ff145b87c0ed116834c59d0daad4
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-25 14:14:50 +0200

    Fix quote state not updating

--- src/containers/Common/Footer/index.js
+++ src/containers/Common/Footer/index.js
@@ -6,7 +6,7 @@ class Footer extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      quote: {}
+      quote: props.quote
     }
   }
 
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Tilt from 'react-tilt'
 import PropTypes from 'prop-types'
 import './styles.css'
 
@@ -7,28 +8,33 @@ class Dashboard extends Component {
     super(props)
     this.state = {
       name: '',
-      quote: {}
+      quote: props.quote
     }
   }
 
   componentWillReceiveProps (props) {
-    this.setState({...props})
+    this.setState({quote: props.quote})
   }
 
   render () {
     const { quote } = this.state
+    console.log(quote)
     return (
       <React.Fragment>
         <section id='home' className='large-header' >
           <canvas id='animated-canvas' />
           <div className='overlay'>
-            <div className='main-title'>
-              {/* <h1 className='typewriter'>{name}</h1>
-              <br />
-              <br /> */}
-              <p><i>{ quote.quote || '' }</i></p>
-              <p><b>{ `-${quote.author || ''}` }</b></p>
-            </div>
+            <Tilt
+              options={{ max: 25 }}
+            >
+              <div className='main-title'>
+                {/* <h1 className='typewriter'>{name}</h1>
+                <br />
+                <br /> */}
+                <p><i>{ quote.quote || '' }</i></p>
+                <p><b>{ `-${quote.author || ''}` }</b></p>
+              </div>
+            </Tilt>
           </div>
         </section>
       </React.Fragment>
