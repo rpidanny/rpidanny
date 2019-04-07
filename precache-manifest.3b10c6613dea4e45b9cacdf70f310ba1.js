dbc269e8f65a8e3ef68da8051cb6e3ac8d66dc35
rpidanny (from Travis CI) <abhishekmaharjan1993@gmail.com>
2019-04-07 20:06:47 +0000

    Deploy rpidanny/rpidanny to github.com/rpidanny/rpidanny.git:gh-pages

--- precache-manifest.3b10c6613dea4e45b9cacdf70f310ba1.js
+++ precache-manifest.3b10c6613dea4e45b9cacdf70f310ba1.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
   },
   {
-    "revision": "2acb6f7770112979fa21",
+    "revision": "d22d7fd658405643cce9",
     "url": "/static/css/main.77ad3f9f.chunk.css"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = [
     "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
-    "revision": "2acb6f7770112979fa21",
-    "url": "/static/js/main.2acb6f77.chunk.js"
+    "revision": "d22d7fd658405643cce9",
+    "url": "/static/js/main.d22d7fd6.chunk.js"
   },
   {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.798c0dfe.chunk.css"
   },
   {
-    "revision": "0cec16a1dafc6c7755966a85b058a36a",
+    "revision": "6106aff7b9688cb525d5a8b25b2a30ae",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0dba02e356cbd025459b2c0a10a7faa1.js"
+  "/precache-manifest.3b10c6613dea4e45b9cacdf70f310ba1.js"
 );
 
 workbox.clientsClaim();
