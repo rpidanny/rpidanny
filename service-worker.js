6721243f0bb138ed3f957829d2dc758a9cc8579a
rpidanny (from Travis CI) <abhishekmaharjan1993@gmail.com>
2020-03-15 20:06:38 +0000

    Deploy rpidanny/rpidanny to github.com/rpidanny/rpidanny.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23bfbe229bf85815208de45e98c2daf3.js"
+  "/precache-manifest.e0ef2a65ffbed90714dd7e773d7a6bb7.js"
 );
 
 workbox.clientsClaim();
