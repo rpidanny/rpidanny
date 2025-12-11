6105c867727729d98932b02acf87b80f18351529
rpidanny <abhishekmaharjan1993@gmail.com>
2025-12-11 19:50:20 +0100

    feat: add particles

--- src/containers/DashboardV2/index.tsx
+++ src/containers/DashboardV2/index.tsx
@@ -3,6 +3,8 @@ import "./styles.css";
 
 import React from "react";
 
+import Particles from "../../components/Particles";
+
 interface DashboardProps {
   name: string;
 }
@@ -12,25 +14,36 @@ const Dashboard: React.FC<DashboardProps> = ({ name }) => {
     <React.Fragment>
       <section id="home" className="large-header">
         <div className="overlay">
-          {/* <Tilt
-            tiltMaxAngleX={25}
-            tiltMaxAngleY={25}
-            scale={1.1}
-          > */}
-          <div className="main-title">
-            <p>
-              <i>Hi, My Name is</i>
-            </p>
-            <div style={{ display: "inline-block" }}>
-              <h1 className="typewriter">{name}</h1>
+          <Particles
+            particleColors={['#ffffff', '#ffffff']}
+            particleCount={200}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={true}
+            disableRotation={false}
+          >
+            {/* <Tilt
+              tiltMaxAngleX={25}
+              tiltMaxAngleY={25}
+              scale={1.1}
+            > */}
+            <div className="main-title">
+              <p>
+                <i>Hi, My Name is</i>
+              </p>
+              <div style={{ display: "inline-block" }}>
+                <h1 className="typewriter">{name}</h1>
+              </div>
+              <br />
+              <br />
             </div>
-            <br />
-            <br />
-          </div>
-          {/* </Tilt> */}
-          <p className="arrow-container">
-            <i className="arrow down" />
-          </p>
+            {/* </Tilt> */}
+            <p className="arrow-container">
+              <i className="arrow down" />
+            </p>
+          </Particles>
         </div>
       </section>
     </React.Fragment>
